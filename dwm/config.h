47e4d108dd0be9b53f8b244c28ee097e712df0f4
Barae <93267708+baraeerriyachy@users.noreply.github.com>
2022-04-16 19:49:31 +0000

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -68,6 +68,7 @@ static const char *voldowncmd[] = { "pactl", "set-sink-volume", "0", "-5%", NULL
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_z,      zoom,           {0} },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
